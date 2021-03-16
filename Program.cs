977420fd137f841e7f23aea70c6dfb7432f4cae6
greedy2119 <greedygreedy2119@gmail.com>
2021-03-16 15:46:07 +0700

    Info Taxi

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai mahasiswa
+            taxi.DriverName = "Jono";
+            taxi.Onduty = true;
+
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool Onduty;
+        public int NumPassenger { get; set; }
+
+        //buat objek mahasiswa
+
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+
+
+            if (Onduty == true)
+            {
+                Console.WriteLine("Onduty: yes", Onduty);
+            }
+            else
+            {
+                Console.WriteLine("Onduty: no", Onduty);
+            }
+            Console.WriteLine("NumPassenger: {0}\n", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang menjemput Penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai Menantar Penumpang\n", DriverName);
+        }
+    }
+}
